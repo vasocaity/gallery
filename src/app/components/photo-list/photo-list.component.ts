83d70d29c96608da6680bf01b49e6b2e9a1725ef
vasocaity <lazy.yoci11@gmail.com>
2018-08-18 13:37:44 +0700

    read data from firebase

--- src/app/components/photo-list/photo-list.component.ts
+++ src/app/components/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  photoList: AngularFireList<any>;
+  photo: any[];
+  constructor(db: AngularFireDatabase) {
+    this.photoList = db.list('albums');
   }
-
+  ngOnInit() {
+    this.photoList.snapshotChanges().pipe(map(actions => {
+      return actions.map(action => (
+        { key: action.key, value: action.payload.val() }
+      )
+      );
+    })).subscribe(items => {
+      this.photo = items;
+    });
+  }
 }
